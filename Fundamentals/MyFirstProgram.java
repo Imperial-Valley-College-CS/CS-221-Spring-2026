2cecd6f5f91e541aa60ab15217c6b184dccde59a
ortoct <octavio.ortiz@imperial.edu>
2026-02-19 19:14:04 -0800

    Initial commit

---
+++ Fundamentals/MyFirstProgram.java
@@ -0,0 +1,10 @@
+public class MyFirstProgram
+{
+   public static void main(String[] args)
+   {
+      int myAge = 38;
+      String myName = "Octavio Ortiz";
+      System.out.print(myName);
+      System.out.println(myAge);
+   }//end main
+}//end class
