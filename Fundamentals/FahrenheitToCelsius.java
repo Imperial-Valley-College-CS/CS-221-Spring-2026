081384bcc419a1c991c921c8d85b103bd76616c8
ortoct <104336410+ortoct@users.noreply.github.com>
2026-02-23 15:12:41 -0800

    FahrenheitToCelsius

---
+++ Fundamentals/FahrenheitToCelsius.java
@@ -0,0 +1,11 @@
+public class FahrenheitToCelsius
+{
+   public static void main(String[] args)
+   {
+      double degF = 109.5;
+      
+      double degC = (5.0/9)*(degF-32);
+      
+      System.out.printf("%.1f degF is %.1f degC",degF,degC);
+   }
+}
