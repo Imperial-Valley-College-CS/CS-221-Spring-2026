a825c4386a6349534d40303e60e7fcda9cc97dc2
ortoct <104336410+ortoct@users.noreply.github.com>
2026-02-23 14:43:05 -0800

    Added Print & PrintF

---
+++ Fundamentals/Print.java
@@ -0,0 +1,9 @@
+public class Print
+{
+   public static void main(String[] args)
+   {
+      System.out.print("Name: Octavio Ortiz\n");
+      System.out.print("Major: Aerospace Engineering\n");
+      System.out.print("Age: 38");
+   }//end main
+}//end class
---
+++ Fundamentals/PrintF.java
@@ -0,0 +1,7 @@
+public class PrintF
+{
+   public static void main(String[] args)
+   {
+      System.out.printf("High School: %s%nGPA: %.2f%nOnly Child: %b", "Calexico", 3.64, false);
+   }//end main
+}//end class
