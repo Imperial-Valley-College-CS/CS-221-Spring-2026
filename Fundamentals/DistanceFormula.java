2fee8e34422b757902ab1ec3fd4ad635c537106d
ortoct <104336410+ortoct@users.noreply.github.com>
2026-02-25 14:34:30 -0800

    Asked user for coordinates

---
+++ Fundamentals/DistanceFormula.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DistanceFormula
+{
+   public static void main(String[] args)
+   {
+      //Create a Scanner object to scan data from user
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.print("Enter location x1: ");
+      double x1 = scan.nextDouble();
+      System.out.print("Enter location y1: ");
+      double y1 = scan.nextDouble();
+      System.out.print("Enter location x2: ");
+      double x2 = scan.nextDouble();
+      System.out.print("Enter location y2: ");
+      double y2 = scan.nextDouble();
+      
+   }//end main
+}//end class
