4627e306a410d956468ef7cad2c5e7e206b2577c
ortoct <104336410+ortoct@users.noreply.github.com>
2026-03-18 15:03:18 -0700

    Updated LeapYears

--- Repetition/LeapYears.java
+++ Repetition/LeapYears.java
@@ -2,11 +2,15 @@ public class LeapYears
 {
    public static void main(String[] args)
    {
-      int year = 1978;
+      int startYear = 2024;      //entered by user
+      int endYear = 2110;        //entered by user
       
-      if( year%400 == 0 || (year%4 == 0 && year%100 != 0))
+      for( int year = startYear; year <= endYear; year++ )
       {
-         System.out.println( year + " is a leap year.");
+         if( year%400 == 0 || (year%4 == 0 && year%100 != 0))
+         {
+            System.out.println( year + " is a leap year.");
+         }
       }
    }
 }
