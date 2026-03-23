dcff496f8ef0f37c520a06bbe016e39b85b87db9
ortoct <104336410+ortoct@users.noreply.github.com>
2026-03-23 15:02:44 -0700

    Added countVowels to methods

---
+++ Methods/CountVowels.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CountVowels
+{
+   public static void main(String[] args)
+   {
+      Scanner scan = new Scanner(System.in);
+      System.out.print("What is your favorite superhero? ");
+      String supe = scan.nextLine();
+      System.out.println( 2*countVowels("iron man") );
+      int y = countVowels("batman");
+      int z = countVowels("doctor strange");
+      System.out.println( "Number of Vowels: " + " " + y + " " + z);
+   }//end main
+   
+   public static int countVowels( String str )
+   {
+      int numVowels = 0;
+      for( int i = 0; i < str.length(); i++ )
+      {
+         char let = str.toLowerCase().charAt(i);
+         if( let == 'a' || let == 'e' || let == 'i' || let == 'o' 
+            || let == 'u' )
+         {
+            numVowels++;  
+         }
+      }
+      return numVowels;
+   }//end countVowels
+}//end class
