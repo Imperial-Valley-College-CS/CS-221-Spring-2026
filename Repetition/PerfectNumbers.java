7c7028a2507c3600110f68d673a04faf04007ead
ortoct <104336410+ortoct@users.noreply.github.com>
2026-03-25 14:03:01 -0700

    PerfectNumbers

---
+++ Repetition/PerfectNumbers.java
@@ -0,0 +1,30 @@
+public class PerfectNumbers
+{
+   public static void main(String[] args)
+   {
+      int x = 1;
+      while ( x <= 10000 )
+      {
+         boolean ans = isPerfect(x);
+         if( ans )
+            System.out.println( x );
+            
+         x++;
+      }
+   }
+   
+   public static boolean isPerfect( int num )
+   {
+      int sum = 0;
+      int x = 1; 
+      while( x <= num/2 )
+      {
+         if( num % x == 0 )
+            sum = sum + x;
+            
+         x++;
+      }
+      
+      return num == sum;
+   }
+}
