286e0837137916fa092edaf1027573f37c48d813
ortoct <104336410+ortoct@users.noreply.github.com>
2026-05-11 15:20:56 -0700

    Added card Image to Scene

--- JavaFX/Solitaire/App.java
+++ JavaFX/Solitaire/App.java
@@ -1,15 +1,28 @@
-public class App
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+
+public class App extends Application
 {
-   private static Card[] deck = new Card[52];
-   private static String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-   private static String[] suits = {"C", "D", "H", "S"};
+   private Card[] deck = new Card[52];
+   private String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+   private String[] suits = {"C", "D", "H", "S"};
+   Group g = new Group();        //Group is a Parent
+   Scene scene = new Scene(g);    //scene has no zero-arg constructor (Scene needs a Parent)
    
-   public static void main(String[] args)
+   //inherit start method from Application class - must be overridden
+   @Override
+   public void start(Stage stage)
    {
       createDeck();
+      g.getChildren().add( deck[51].getImView() );
+      stage.setScene( scene );
+      stage.show();     //stage needs a scene
    }
    
-   public static void createDeck()
+   
+   public void createDeck()
    {
       int j = 0;        //index value of ranks
       int k = 0;        //index value of suits
@@ -17,7 +30,7 @@ public class App
       {
          deck[i] = new Card(ranks[j], suits[k]);
          j++;
-         if( i == 12 || i == 25 || i == 38 )
+         if( i == 12 || i == 25 || i == 38 )    //try (i-k)%12 == 0
          {
             j = 0;      //reset j back to 0
             k++;        //increment k by 1
--- JavaFX/Solitaire/Card.java
+++ JavaFX/Solitaire/Card.java
@@ -1,11 +1,23 @@
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Card
 {
    private String rank, suit;    //5-C (rank = 5, suit = C)
+   private Image image;
+   private ImageView imageView;
    
    public Card(String r, String s)
    {
       this.rank = r;
       this.suit = s;
+      this.image = new Image("cards/"+this.rank + "-" + this.suit + ".png");
+      this.imageView = new ImageView(image);
+   }
+   
+   public ImageView getImView()
+   {
+      return this.imageView;
    }
    
    @Override
