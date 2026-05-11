424a89914034f1609c0c55d1b7009ec8bf85ec96
ortoct <104336410+ortoct@users.noreply.github.com>
2026-05-11 14:54:50 -0700

    Updated App to createDeck

--- JavaFX/Solitaire/App.java
+++ JavaFX/Solitaire/App.java
@@ -1,4 +1,30 @@
 public class App
 {
-   private Card[] deck = new Card[52];
+   private static Card[] deck = new Card[52];
+   private static String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+   private static String[] suits = {"C", "D", "H", "S"};
+   
+   public static void main(String[] args)
+   {
+      createDeck();
+   }
+   
+   public static void createDeck()
+   {
+      int j = 0;        //index value of ranks
+      int k = 0;        //index value of suits
+      for( int i = 0; i < deck.length; i++ )
+      {
+         deck[i] = new Card(ranks[j], suits[k]);
+         j++;
+         if( i == 12 || i == 25 || i == 38 )
+         {
+            j = 0;      //reset j back to 0
+            k++;        //increment k by 1
+         }
+      }
+      
+      for( Card c : deck )
+         System.out.println( c );
+   }
 }
--- JavaFX/Solitaire/Card.java
+++ JavaFX/Solitaire/Card.java
@@ -1,3 +1,16 @@
 public class Card
 {
+   private String rank, suit;    //5-C (rank = 5, suit = C)
+   
+   public Card(String r, String s)
+   {
+      this.rank = r;
+      this.suit = s;
+   }
+   
+   @Override
+   public String toString()
+   {
+      return this.rank + "-" + this.suit;
+   }
 }
